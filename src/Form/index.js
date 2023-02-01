94e2f56a177b17c67eaf94c009aa9d06f77e682a
Kuba Tuta <tuta.jakub@gmail.com>
2023-02-01 11:23:29 +0100

    focus on initial load and on button click

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useEffect } from "react";
 import "./style.css";
 
 const Form = ({addNewTask}) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
+
+    useEffect(()=> inputRef.current.focus(), []);
+
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        inputRef.current.focus();
     };
 
     return (
-        <form className="form" onSubmit={onFormSubmit}>
+        <form className="form" onSubmit={onFormSubmit} >
             <input
                 value={newTaskContent}
                 className="form__input"
                 type="text"
                 placeholder="Co jest do zrobienia?"
                 onChange={(event) => setNewTaskContent(event.target.value)}
+                ref={inputRef}
             />
             <button className="form__addButton">Dodaj zadanie</button>
         </form>
